71a32e070604c5fedef62e4f22882d8e4738a000
GujralNaman <gujralnaman2002@gmail.com>
2023-06-06 18:29:20 +0530

    background changed

--- assets/index.js
+++ assets/index.js
@@ -6,6 +6,15 @@ Array.from(buttons).forEach((button)=>{
             string = eval(string);
             document.querySelector('input').value = string;
         }
+        else if(e.target.innerHTML == 'M+'){
+            alert("Not enabled Yet")
+        }
+        else if(e.target.innerHTML == 'M-'){
+            alert("Not enabled Yet")
+        }
+        else if(e.target.innerHTML == '%'){
+            alert("Not enabled Yet")
+        }
         else if(e.target.innerHTML == 'C'){
             string = "";
             document.querySelector('input').value = string;
